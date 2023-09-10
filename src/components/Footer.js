58cbf02f82b138f84eb5c63453c9749160800680
noceo <paulschade20@gmail.com>
2023-09-10 10:52:21 +0200

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-items">
+        <small>@2023</small>
+      </div>
+    </footer>
+  );
+}
