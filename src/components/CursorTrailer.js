baf89cd2355bc02f96ce4de6782ca3c120e15c71
noceo <paulschade20@gmail.com>
2024-01-31 18:44:33 +0100

    fixed lagging cursor on safari

--- src/components/CursorTrailer.js
+++ src/components/CursorTrailer.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { detectMobileAndTablet } from "@/utils/detect-mobile";
 import { useEffect, useRef } from "react";
 
 export default function CursorTrailer() {
@@ -8,38 +9,53 @@ export default function CursorTrailer() {
   const mouseDownRef = useRef();
   const interactingRef = useRef();
   const hideRef = useRef();
+  const mousePos = useRef({ x: 0, y: 0 });
+  const trailingCursorPos = useRef({ x: 0, y: 0 });
+  const animationFrameRef = useRef();
 
   useEffect(() => {
     const onMouseMove = (e) => {
-      const x = e.clientX - cursorContainerRef.current.offsetWidth / 2;
-      const y = e.clientY - cursorContainerRef.current.offsetHeight / 2;
+      const { clientX, clientY } = e;
+      mousePos.current = {
+        x: clientX - cursorContainerRef.current.offsetWidth / 2,
+        y: clientY - cursorContainerRef.current.offsetWidth / 2,
+      };
+    };
 
+    const rafRender = () => {
+      const mouseX = mousePos.current.x;
+      const mouseY = mousePos.current.y;
+      const trailingX = trailingCursorPos.current.x;
+      const trailingY = trailingCursorPos.current.y;
+      const diffX = mouseX - trailingX;
+      const diffY = mouseY - trailingY;
+      trailingCursorPos.current = {
+        x: trailingX + diffX / 5,
+        y: trailingY + diffY / 5,
+      };
       const scale =
         (interactingRef.current ? 4 : 1) * (mouseDownRef.current ? 2 : 1);
-
-      cursorContainerRef.current.style.transform = `translate(${x}px, ${y}px)`;
+      cursorContainerRef.current.style.transform = `translate3d(${trailingX}px, ${trailingY}px, 0)`;
       cursorRef.current.style.transform = `scale(${scale})`;
-      if (hideRef.current) cursorContainerRef.current.style.opacity = 0;
-      else cursorContainerRef.current.style.opacity = 1;
-
-      // anime({
-      //   targets: cursorRef.current,
-      //   translateX: x,
-      //   translateY: y,
-      //   duration: 400,
-      // });
-
-      // const keyframes = {
-      //   transform: `translate(${x}px, ${y}px) scale(${
-      //     (interacting ? 4 : 1) * (mousedown ? 4 : 1)
-      //   })`,
-      // };
-
-      // cursorRef.current.animate(keyframes, {
-      //   duration: 800,
-      //   fill: "forwards",
-      // });
+      animationFrameRef.current = requestAnimationFrame(rafRender);
     };
+
+    // const onMouseMove = (e) => {
+    //   const x = e.clientX - cursorContainerRef.current.offsetWidth / 2;
+    //   const y = e.clientY - cursorContainerRef.current.offsetHeight / 2;
+
+    //   const delayX = x * 0.8 - lastPos.current.x * 0.2;
+    //   const delayY = y * 0.8 - lastPos.current.y * 0.2;
+    //   const scale =
+    //     (interactingRef.current ? 4 : 1) * (mouseDownRef.current ? 2 : 1);
+
+    //   cursorContainerRef.current.style.transform = `translate(${delayX}px, ${delayY}px)`;
+    //   cursorRef.current.style.transform = `scale(${scale})`;
+    //   if (hideRef.current) cursorContainerRef.current.style.opacity = 0;
+    //   else cursorContainerRef.current.style.opacity = 1;
+
+    //   lastPos.current = { x, y };
+    // };
 
     const onMouseDown = () => {
       mouseDownRef.current = true;
@@ -66,6 +82,13 @@ export default function CursorTrailer() {
     window.addEventListener("mouseup", (e) => {
       onMouseUp(e);
     });
+
+    if (detectMobileAndTablet())
+      cursorContainerRef.current.style.display = "none";
+    else {
+      cursorContainerRef.current.style.opacity = 1;
+      rafRender();
+    }
   }, []);
 
   return (
