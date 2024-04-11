2360e7cbbd23047a4db46705369ebbfe4bcbd3da
noceo <paulschade20@gmail.com>
2024-04-10 21:25:12 -0400

    added cv link to about page

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -40,14 +40,14 @@ export default function About() {
               </div>
               <h4 className="heading3">{t("skills.headline")}</h4>
               <p className="mb-3">{t("skills.text")}</p>
-              {/* <Button
+              <Button
                 className="mb-7"
                 asLink={true}
-                href="/"
+                href="https://drive.google.com/file/d/1r3UV4nHxDCrIAdCjmO1ojhAlrPxIDX0V/view?usp=sharing"
                 icon={<ArrowRight />}
               >
                 {t("skills.buttonText")}
-              </Button> */}
+              </Button>
               <h4 className="heading3">{t("contact.headline")}</h4>
               <p className="mb-3">{t("contact.text")}</p>
               <Button className="mb-7" icon={<ArrowRight />}>
